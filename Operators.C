0743f9149f45ed88d1ee3e2740b60682063be2ce
Hayasri <rajanhayasri@gmail.com>
2026-06-09 17:08:52 +0530

    Operators.C

---
+++ Operators.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("Enter the value of a : ");
+scanf("%d",&a);
+printf("Enter the value of b : ");
+scanf("%d",&b);
+printf("sum = a+b %d\n",a+b);
+printf("sub = a-b %d\n",a-b);
+printf("mul= a*b %d\n",a*b);
+printf("div = a/b %d\n",a/b);
+printf("and = a&b %d\n",a&b);
+printf("or= a|b %d\n",a|b);
+return 0;
+}
