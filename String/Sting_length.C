4f55d6c06f1afcc2617035bd15c8c9001ac6a688
Hayasri <rajanhayasri@gmail.com>
2026-06-12 12:52:21 +0530

    Create Sting_length.C

---
+++ String/Sting_length.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main (){
+    char name[] =  "Hayasri";
+    int len;
+    len = strlen(name);
+    printf("length of the string is : %d \n",len);
+    return 0;
+}
+//output 
+length of the string is : 7 
