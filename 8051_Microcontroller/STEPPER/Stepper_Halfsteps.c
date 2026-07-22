5062c73b6969941f6a5812dbedbe3a1043e6e9a0
Hayasri <rajanhayasri@gmail.com>
2026-07-22 17:50:20 +0530

    Create Stepper_Halfsteps.c

---
+++ 8051_Microcontroller/STEPPER/Stepper_Halfsteps.c
@@ -0,0 +1,25 @@
+#include <reg51.h>
+#define stepper_motor P2
+void delay(){
+	unsigned int i,j;
+	for(i=0;i<100;i++){
+		for(j=0;j<1275;j++){
+		}
+	}
+}
+unsigned char seg_code[]={
+	0x08,0x0C,0x04,0x06,0x02,0x03,0x01,0x09
+};
+void main()
+{
+	unsigned int i;
+	while(1){
+		for(i=0;i<8;i++){
+			P2=seg_code[i];
+			delay();
+		}
+	}
+}
+		
+		
+	
